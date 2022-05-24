02eeb060be44ec97c019b659f67626f36de15261
Ose-Augustine <osebeckley@gmail.com>
2022-05-24 20:44:24 +0100

    Handler for partial sting search for artists by name and by venue

--- starter_code/app.py
+++ starter_code/app.py
@@ -228,16 +228,18 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
+  search_term = request.form.get('search_term')
+  if ',' in term :
+    term = term.replace(' ','').split(',')
+    city = term[0]
+    state = term[1] 
+    result = Artist.query.filter(Artist.city.ilike(f'%{city}%'),Artist.state.ilike(f'%{state}%')).all()
+  else:
+    result = Artist.query.filter(Artist.name.ilike(f'%{term}%')).all()
+  counter = len(result)
   response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+    "count": counter,
+    "data": result 
   }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
